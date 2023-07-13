e79f1cbf44e8b8c54f96b03b5374b1e75d9555ad
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-13 20:33:05 +0530

    +1

--- 3. Components and Props, One Way Data binding in ReactJS, CSS styling in ReactJS, Virtual Dom/my-react-app/src/App.jsx
+++ 3. Components and Props, One Way Data binding in ReactJS, CSS styling in ReactJS, Virtual Dom/my-react-app/src/App.jsx
@@ -1,19 +1,7 @@
 import "./App.css";
-import Section from "./components/Section";
-import Welcome from "./components/Welcome";
 
 function App() {
-  const name = "Priyanshu";
-
-  return (
-    <>
-      <Welcome name="Eram" rollNo="1" />
-      <Welcome name="Kareena" rollNo="2" />
-      <Welcome name="Santoshi" rollNo="3" />
-      <Welcome name={name} rollNo="4" />
-      <Section />
-    </>
-  );
+  return <>Hello ReactJS</>;
 }
 
 export default App;
--- 3. Components and Props, One Way Data binding in ReactJS, CSS styling in ReactJS, Virtual Dom/my-react-app/src/components/Section.jsx
+++
@@ -1,9 +0,0 @@
-const Section = () => {
-  return (
-    <>
-      <h2>Heloo from section!</h2>
-    </>
-  );
-};
-
-export default Section;
