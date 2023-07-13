3b5f9b0ee1bea1015a36e19f8105e3a904d897dd
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-13 20:29:31 +0530

    lecture 3

---
+++ 3. Components and Props, One Way Data binding in ReactJS, CSS styling in ReactJS, Virtual Dom/my-react-app/src/components/Section.jsx
@@ -0,0 +1,9 @@
+const Section = () => {
+  return (
+    <>
+      <h2>Heloo from section!</h2>
+    </>
+  );
+};
+
+export default Section;
---
+++ 3. Components and Props, One Way Data binding in ReactJS, CSS styling in ReactJS, Virtual Dom/my-react-app/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+const Welcome = ({ name, rollNo }) => {
+  return (
+    <>
+      <h1>Welcome {name} !</h1>
+      <p>Roll No {rollNo}</p>
+    </>
+  );
+};
+
+export default Welcome;
