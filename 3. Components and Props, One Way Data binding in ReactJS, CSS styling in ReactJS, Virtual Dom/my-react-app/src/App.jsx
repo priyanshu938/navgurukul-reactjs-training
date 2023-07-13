c5a861495288a08fda38989e9d8c37d3c735834d
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-13 21:56:22 +0530

    +1

--- 3. Components and Props, One Way Data binding in ReactJS, CSS styling in ReactJS, Virtual Dom/my-react-app/src/App.jsx
+++ 3. Components and Props, One Way Data binding in ReactJS, CSS styling in ReactJS, Virtual Dom/my-react-app/src/App.jsx
@@ -6,14 +6,11 @@ function App() {
     roll = "5";
   return (
     <>
-      {/*
- <Welcome name="Anzum" roll="1" />
-       <Welcome name="Jyoti" roll="2" />
-       <Welcome name="Riya" roll="3" />
-       <Welcome name="Eram" roll="4" />
-       <Welcome name={name} roll={roll} />
-        */}
-
+      <Welcome name="Anzum" roll="1" />
+      <Welcome name="Jyoti" roll="2" />
+      <Welcome name="Riya" roll="3" />
+      <Welcome name="Eram" roll="4" />
+      <Welcome name={name} roll={roll} />
       <Welcome>
         <h1>Hello I am ReactJS!</h1>
         <h2>skhcskj</h2>
