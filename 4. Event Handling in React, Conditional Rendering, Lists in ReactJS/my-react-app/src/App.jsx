6cf944ffb9f29a863a924b231eaa7759f4a555c8
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-14 21:54:32 +0530

    event completed

--- 4. Event Handling in React, Conditional Rendering, Lists in ReactJS/my-react-app/src/App.jsx
+++ 4. Event Handling in React, Conditional Rendering, Lists in ReactJS/my-react-app/src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <>Hello ReactJS</>;
+  const clicked = (event, name) => {
+    console.log(event.type, name);
+    alert("Hello i am clicked");
+  };
+
+  const inputBox = (e) => {
+    console.log(e.target.value);
+  };
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <input type="text" name="" id="" onChange={inputBox} />
+      <button onClick={(e) => clicked(e, "Priyanshu")}>I am a button</button>
+    </div>
+  );
 }
 
 export default App;
