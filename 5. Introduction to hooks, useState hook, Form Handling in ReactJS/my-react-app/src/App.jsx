dfee36af5fb37179f69deab3558712cc617dc733
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-15 23:02:05 +0530

    form handling done

--- 5. Introduction to hooks, useState hook, Form Handling in ReactJS/my-react-app/src/App.jsx
+++ 5. Introduction to hooks, useState hook, Form Handling in ReactJS/my-react-app/src/App.jsx
@@ -1,19 +1,24 @@
 import "./App.css";
+import Forms from "./components/Forms";
 import State from "./components/State";
 
 function App() {
   return (
     <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        gap: "2rem",
-      }}
+    //   style={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     height: "100vh",
+    //     gap: "2rem",
+    //   }}
     >
-      <State />
+      {/*
+        <State />
+        
+        */}
+      <Forms />
     </div>
   );
 }
---
+++ 5. Introduction to hooks, useState hook, Form Handling in ReactJS/my-react-app/src/components/Forms.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const Forms = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        gap: "1rem",
+      }}
+    >
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        name=""
+        id="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        name=""
+        id="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Forms;
