63ef780b9c7e98930ad603492c27b8dbd2bb01e2
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-18 22:34:58 +0530

    form project completed

--- 6. Project 1 : Form Project/my-react-app/src/components/loginForm/LoginForm.jsx
+++ 6. Project 1 : Form Project/my-react-app/src/components/loginForm/LoginForm.jsx
@@ -1,8 +1,74 @@
-import React from "react";
-import style from "./LoginForm.module.css";
+import React, { useState } from "react";
+import styles from "./LoginForm.module.css";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const LoginForm = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordErrorMessage, setShowPasswordErrorMessage] =
+    useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password.length < 6) {
+      setShowPasswordErrorMessage(true);
+      return;
+    }
+    setShowPasswordErrorMessage(false);
+    console.log(email, password);
+  };
+  return (
+    <>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <h1>🔏Login🔏</h1>
+        <div className={styles.inputContainer}>
+          <EmailIcon className={styles.icon} />
+          <input
+            type="email"
+            placeholder="Enter email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className={styles.inputAndErrorContainer}>
+          <div className={styles.inputContainer}>
+            <LockIcon className={styles.icon} />
+            <input
+              type={showPassword ? "password" : "text"}
+              placeholder="Enter password..."
+              className={styles.passwordInput}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+
+            {!showPassword ? (
+              <VisibilityIcon
+                className={styles.icon}
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            ) : (
+              <VisibilityOffIcon
+                className={styles.icon}
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            )}
+          </div>
+          {showPasswordErrorMessage && (
+            <p className={styles.errorMessage}>Password too short!</p>
+          )}
+        </div>
+        <button className={styles.loginButton} type="submit">
+          Login
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default LoginForm;
