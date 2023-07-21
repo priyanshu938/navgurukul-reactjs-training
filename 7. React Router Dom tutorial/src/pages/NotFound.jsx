41d1f17677c7084ef0c752f38332b031abab7d6d
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-21 22:23:35 +0530

    +1

--- 7. React Router Dom tutorial/src/pages/NotFound.jsx
+++ 7. React Router Dom tutorial/src/pages/NotFound.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>404 not found</div>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/about");
+  };
+  return (
+    <div>
+      404 not found
+      <button onClick={handleClick}>Back to home</button>
+    </div>
+  );
 };
 
 export default NotFound;
